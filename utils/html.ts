b2e53c485faaffb6459b9191a0fdb92b6c240b76
Alexander Mashinskiy <dwarlin@mail.ru>
2025-09-17 21:11:21 +0300

    Убрал лог

--- utils/html.ts
+++ utils/html.ts
@@ -2,7 +2,6 @@ export const htmlStr = `
   <div id="hotToursContainer">
     <script src="https://ui.sletat.ru/module-4.0/core.js" charset="utf-8"></script>
     <script>
-    console.log(window.sletat)
       window?.sletat?.FrameHot.$create({
         useCard: false,
         enabledCurrencies: ["RUB"]
